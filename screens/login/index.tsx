2fa957476c868f9e3a80403e0450ea33583a4938
lucasaragao29 <lucasaragao2904@gmail.com>
2023-12-20 22:09:28 -0300

    telalogin

--- screens/login/index.tsx
+++ screens/login/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { EmailInput, SenhaInput, BtnLogin, BtnTxt, Container, ContInpt, Txt, IconCont} from './style';
 import { Image } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import IconMat from 'react-native-vector-icons/MaterialIcons';
+import IconEvil from 'react-native-vector-icons/EvilIcons';
 
 export default function LoginScreen() {
-  const IconSend = <Icon name="send" size={45} color="black" />;
+  const IconSend = <IconMat name="send" size={45} color="black" />;
+  const IconLogin = <IconEvil name="user" size={45} color="black" />;
+  const IconPass = <IconMat name="lock-outline" size={45} color="black" />;
   return (
     <Container>
       <Image
