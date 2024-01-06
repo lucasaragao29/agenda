1ce18403bb2c921253e904c38f0be2284297d66a
lucasaragao29 <lucasaragao2904@gmail.com>
2024-01-06 19:11:53 -0300

    TelaCalendario

--- screens/calendario/components/calendar.tsx
+++ screens/calendario/components/calendar.tsx
@@ -1,8 +1,53 @@
+import { useState } from "react";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 
+LocaleConfig.locales['fr'] = {
+  monthNames: [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre'
+  ],
+  monthNames: [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ],
+  monthNamesShort: ['Jan.', 'Fev.', 'Mar.', 'Abr.', 'Maio', 'Jun.', 'Jul.', 'Aug', 'Set.', 'Out.', 'Nov.', 'Dez.'],
+  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'],
+  dayNamesShort: ['Dom.', 'Seg.', 'Ter.', 'Qua.', 'Qui.', 'Sex.', 'Sab.'],
+  today: "Hoje"
+};
+LocaleConfig.defaultLocale = 'fr';
+
 export default function Calendario(){
+const [selected, setSelected] = useState('');
   return(
     <Calendar
+    onDayPress={day => {
+      setSelected(day.dateString);
+    }}
+    markedDates={{
+      [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
+    }}
+
       style={{
         borderWidth: 1,
         height: 400,
@@ -11,13 +56,12 @@ export default function Calendario(){
         borderRadius:15,
       }}
       theme={{
-        backgroundColor:'#294380',
-        calendarBackground: '#294380',
-        textSectionTitleColor: '#b6c1cd',
+        backgroundColor: '#294380',
+        textSectionTitleColor: '#000',
         selectedDayBackgroundColor: '#00adf5',
         selectedDayTextColor: '#ffffff',
         todayTextColor: '#00adf5',
-        dayTextColor: '#fff',
+        dayTextColor: '#000',
         textDisabledColor: '#d9e'
       }}
       />
